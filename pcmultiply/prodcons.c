f1eed89a9caee6da7500c4b6cd4203b7b52da3f2
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2025-03-02 19:23:03 -0800

    Memory leak fixed, hopefully

--- pcmultiply/prodcons.c
+++ pcmultiply/prodcons.c
@@ -44,6 +44,7 @@ Matrix * get()
 // Matrix PRODUCER worker thread
 void *prod_worker(void *arg)
 {
+	Matrix *m1 = NULL;
 	ProdConsStats *prod_stats = (ProdConsStats *) arg;
 	prod_stats->sumtotal = 0;
 	prod_stats->matrixtotal = 0;
@@ -61,7 +62,6 @@ void *prod_worker(void *arg)
 
 	while (get_cnt(counters_strut.prod) < NUMBER_OF_MATRICES) {
 		pthread_mutex_lock(&mutex);
-		Matrix *m1;
 		while ((get_cnt(counters_strut.prod) - get_cnt(counters_strut.cons)) >= BOUNDED_BUFFER_SIZE && get_cnt(counters_strut.prod) < NUMBER_OF_MATRICES) {
 			pthread_cond_wait(&prod_cond, &mutex);
 		}
@@ -99,7 +99,7 @@ void *cons_worker(void *arg)
 	pthread_mutex_unlock(&mutex);
 
 	while (get_cnt(counters_strut.cons) < NUMBER_OF_MATRICES) {
-		Matrix *m1, *m2, *m3;
+		Matrix *m1 = NULL, *m2 = NULL, *m3 = NULL;
 		pthread_mutex_lock(&mutex);
 		while ( (get_cnt(counters_strut.prod) - get_cnt(counters_strut.cons)) <= 0  && get_cnt(counters_strut.cons) < NUMBER_OF_MATRICES) {
 			pthread_cond_wait(&cons_cond, &mutex);
@@ -114,6 +114,7 @@ void *cons_worker(void *arg)
 		}
 
 		if (get_cnt(counters_strut.cons) >= NUMBER_OF_MATRICES) {
+			if (m1) FreeMatrix(m1);
 			pthread_mutex_unlock(&mutex);
 			break;
 		}
@@ -131,9 +132,13 @@ void *cons_worker(void *arg)
 		}
 		pthread_mutex_unlock(&mutex);
 
-		if (m1 != NULL && m2 != NULL) m3 = MatrixMultiply(m1, m2);
+		if (m1 && m2) m3 = MatrixMultiply(m1, m2);
 
-		while (m3 == NULL && get_cnt(counters_strut.cons) < NUMBER_OF_MATRICES) {
+		while (!m3 && get_cnt(counters_strut.cons) < NUMBER_OF_MATRICES) {
+			if (m2) {
+				FreeMatrix(m2);
+				m2 = NULL;
+			}
 			pthread_mutex_lock(&mutex);
 			while ( (get_cnt(counters_strut.prod) - get_cnt(counters_strut.cons)) <= 0 && get_cnt(counters_strut.cons) < NUMBER_OF_MATRICES) {
 				pthread_cond_wait(&cons_cond, &mutex);
@@ -145,7 +150,9 @@ void *cons_worker(void *arg)
 				bigmatrix[BOUNDED_BUFFER_SIZE - get_cnt(counters_strut.prod) + get_cnt(counters_strut.cons) - 1] = NULL;
 				pthread_cond_signal(&prod_cond);
 			}
-			m3 = MatrixMultiply(m1, m2);
+			if (m2) {
+				m3 = MatrixMultiply(m1, m2);
+			}
 			pthread_mutex_unlock(&mutex);
 		}
 
@@ -163,6 +170,8 @@ void *cons_worker(void *arg)
 			pthread_mutex_unlock(&mutex);
 		}
 
+		if (m1) FreeMatrix(m1);
+		if (m2) FreeMatrix(m2);
 
 	}
 
